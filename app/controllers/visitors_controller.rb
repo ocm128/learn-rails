1ab3d1f5d9e19fae1b0f00cc251c57d089757b5f
kalasni <kalassni@gmail.com>
2016-01-05 20:36:09 +0100

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new  # @owner es una variable de instancia disponible en la vista
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
